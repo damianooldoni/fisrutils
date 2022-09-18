3c5ede92486b49c853f79737636097c4d4e08fe1
Damiano Oldoni <damiano.oldoni@inbo.be>
2022-09-18 15:54:38 +0200

    Finalize tests about species and locality

--- tests/testthat/test-get_pop_evol.R
+++ tests/testthat/test-get_pop_evol.R
@@ -13,10 +13,35 @@ test_that("df contains all required columns", {
 })
 
 test_that("species is one of the species in df", {
+  # wrong species
   testthat::expect_error(
     get_pop_evol(df = pop_dyn, species = "dog", locality = "Flanders"),
     paste("Invalid value for species parameter:",
           "dog.\nValid inputs are: wild boar and deer."
+    )
+  )
+  # NULL is not allowed
+  testthat::expect_error(
+    get_pop_evol(df = pop_dyn, species = NULL, locality = "Flanders"),
+    paste("Invalid value for species parameter:",
+          "NULL.\nValid inputs are: wild boar and deer."
           )
+  )
+})
+
+test_that("locality is one of the localities in df", {
+  # wrong locality
+  testthat::expect_error(
+    get_pop_evol(df = pop_dyn, species = "deer", locality = "Vidalengo"),
+    paste("Invalid value for locality parameter:",
+          "Vidalengo.\nValid inputs are: Flanders, Sweden and Wallonia."
+    )
+  )
+  # NULL is not allowed
+  testthat::expect_error(
+    get_pop_evol(df = pop_dyn, species = "deer", locality = NULL),
+    paste("Invalid value for locality parameter:",
+          "NULL.\nValid inputs are: Flanders, Sweden and Wallonia."
+    )
   )
 })
